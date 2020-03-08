a1ae3c27104ace335ae9e57504dec9c00fd76982
Sutikshan <s.anand.dubey@gmail.com>
2020-03-08 22:25:51 +1100

    async fetching via useEffect

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
 
-import dummyData from './dummy-data';
-
 import './styles.scss';
+import endpoint from './endpoint';
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    setCharacters([]);
+
+    fetch(`${endpoint}/characters`)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(Object.values(data.characters));
+        setLoading(false);
+      })
+      .catch(exc => {
+        console.error(exc.message);
+        setError(exc);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div className="Application">
@@ -19,7 +38,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p className="loading">Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>

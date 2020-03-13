0e19e952b492fca5779f4aec59520feddab02ac1
Sutikshan <s.anand.dubey@gmail.com>
2020-03-14 08:55:28 +1100

    useReducer is better when more than 3 useSTate

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,27 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
+
+const ActionTypes = {
+  FETCHING: 'FETCHING',
+  RESPONSE_COMPLETE: 'RESPONSE_COMPLETE',
+  ERROR: 'ERROR',
+};
+
+const reducer = (state, action) => {
+  if (action.type === ActionTypes.FETCHING) {
+    return {
+      loading: true,
+      error: null,
+      characters: [],
+    };
+  }
+
+  if (action.type === ActionTypes.RESPONSE_COMPLETE) {
+    return {
+      loading: false,
+      error: null,
+      characters: action.payload,
+    };
+  }
+
+  if (action.type === ActionTypes.ERROR) {
+    return {
+      loading: false,
+      error: action.payload,
+      characters: [],
+    };
+  }
+
+  return state;
+};
 
 const useFetch = (endpoint, formatData) => {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {});
 
   useEffect(() => {
-    setLoading(true);
-    setError(null);
-    setCharacters([]);
+    dispatch({ type: ActionTypes.FETCHING });
 
     fetch(endpoint)
       .then(response => response.json())
       .then(data => {
-        setCharacters(formatData(data));
-        setLoading(false);
+        dispatch({
+          type: ActionTypes.RESPONSE_COMPLETE,
+          payload: formatData(data),
+        });
       })
       .catch(exc => {
         console.error(exc.message);
-        setError(exc);
-        setLoading(false);
+        dispatch({ type: ActionTypes.ERROR, payload: exc.message });
       });
   }, [endpoint, formatData]);
+
+  const { loading, error, characters } = state;
 
   return [loading, error, characters];
 };
